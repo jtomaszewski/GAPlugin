7d951a274e5f7034b51766cd60f233599ddab8d1
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2014-03-17 19:09:40 +0100

    reverting changes on plugin removing v2 for android

--- src/android/GAPlugin.java
+++ src/android/GAPlugin.java
@@ -15,7 +15,8 @@ public class GAPlugin extends CordovaPlugin {
         if (action.equals("initGA")) {
             try {
                 tracker = ga.getTracker(args.getString(0));
-                GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));
+                /*GAServiceManager.getInstance().setDispatchPeriod(args.getInt(1));*/
+                GAServiceManager.getInstance().setLocalDispatchPeriod(args.getInt(1));
                 callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
                 return true;
             } catch (final Exception e) {
@@ -23,7 +24,8 @@ public class GAPlugin extends CordovaPlugin {
             }
         } else if (action.equals("exitGA")) {
             try {
-                GAServiceManager.getInstance().dispatch();
+                /*GAServiceManager.getInstance().dispatch(); */
+                GAServiceManager.getInstance().dispatchLocalHits();
                 callback.success("exitGA");
                 return true;
             } catch (final Exception e) {
@@ -31,7 +33,8 @@ public class GAPlugin extends CordovaPlugin {
             }
         } else if (action.equals("trackEvent")) {
             try {
-                tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3)); 
+                /*tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));  */
+                tracker.send(MapBuilder.createEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3)).build());
                 callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
                 return true;
             } catch (final Exception e) {
@@ -39,7 +42,8 @@ public class GAPlugin extends CordovaPlugin {
             }
         } else if (action.equals("trackPage")) {
             try {
-                tracker.sendView(args.getString(0));
+                /*tracker.sendView(args.getString(0));*/
+                tracker.send(MapBuilder.createAppView().set(Fields.SCREEN_NAME,args.getString(0)).build());
                 callback.success("trackPage - url = " + args.getString(0));
                 return true;
             } catch (final Exception e) {
@@ -47,8 +51,8 @@ public class GAPlugin extends CordovaPlugin {
             }
         } else if (action.equals("setVariable")) {
             try {
-                tracker.setCustomDimension(args.getInt(0), args.getString(1));
-                
+                /*tracker.setCustomDimension(args.getInt(0), args.getString(1));*/
+                tracker.set(Fields.customDimension(args.getInt(0)), args.getString(1));
 
                 callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
                 return true;
@@ -58,7 +62,8 @@ public class GAPlugin extends CordovaPlugin {
         }
         else if (action.equals("setDimension")) {
             try {
-                tracker.setCustomDimension(args.getInt(0), args.getString(1));
+                /*tracker.setCustomDimension(args.getInt(0), args.getString(1));*/
+                tracker.set(Fields.customDimension(args.getInt(0)), args.getString(1));
                 callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
@@ -67,7 +72,8 @@ public class GAPlugin extends CordovaPlugin {
         }
         else if (action.equals("setMetric")) {
             try {
-                tracker.setCustomMetric(args.getInt(0), args.getLong(1));
+                /*tracker.setCustomMetric(args.getInt(0), args.getLong(1));*/
+                tracker.set(Fields.customMetric(args.getInt(0)), args.getString(1));
                 callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
                 return true;
             } catch (final Exception e) {
@@ -76,7 +82,7 @@ public class GAPlugin extends CordovaPlugin {
         }
         else if (action.equals("trackSocial")){
             try{
-                //tracker.send(MapBuilder.createSocial(args.getString(0),args.getString(1),args.getString(2)).build());
+                tracker.send(MapBuilder.createSocial(args.getString(0),args.getString(1),args.getString(2)).build());
                 callback.success("trackSocial - Network = " + args.getString(0) + "; action = " + args.getString(1) + "; target = " + args.getString(2));
             } catch(final Exception e){
                 callback.error(e.getMessage());
@@ -84,13 +90,13 @@ public class GAPlugin extends CordovaPlugin {
         }
         else if (action.equals("trackEcommerceTransaction")){
             try{
-                /*tracker.send(MapBuilder.createTransaction(args.getString(0),       // (String) Transaction ID
+                tracker.send(MapBuilder.createTransaction(args.getString(0),       // (String) Transaction ID
                                                           args.getString(1),       // (String) Affiliation
                                                           args.getDouble(2),       // (Double) Order revenue
                                                           args.getDouble(3),       // (Double) Tax
                                                           args.getDouble(4),       // (Double) Shipping
                                                           args.getString(5)        // (String) Currency code
-                                                        ).build()); */
+                                                        ).build());
 
                 callback.success("trackSocial - Transaction ID = "+ args.getString(0) +
                                  " Affiliation "+args.getString(1) +
@@ -106,14 +112,14 @@ public class GAPlugin extends CordovaPlugin {
         }
         else if (action.equals("trackEcommerceItem")){
             try{
-                /*tracker.send(MapBuilder.createItem( args.getString(0),          // (String) Transaction ID
+                tracker.send(MapBuilder.createItem( args.getString(0),          // (String) Transaction ID
                                                     args.getString(1),          // (String) Product name
                                                     args.getString(2),          // (String) Product SKU
                                                     args.getString(3),          // (String) Product category
                                                     args.getDouble(4),          // (Double) Product price
                                                     args.getLong(5),            // (Long) Product quantity
                                                     args.getString(6)           // (String) Currency code
-                                                  ).build());*/
+                                                  ).build());
 
                 callback.success("trackSocial - Transaction ID = "+ args.getString(0) +
                                  " Name "+args.getString(1) +
